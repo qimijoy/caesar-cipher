904f3fe0956d1b26d4f7b23fca53c2b1c377be95
qimijoy <qimijoy@yandex.ru>
2023-08-29 08:49:57 +0300

    gulp task styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
-function defaultTask(cb) {
+import gulp from 'gulp';
+const { src, dest } = gulp;
+
+import cleanCSS from 'gulp-clean-css';
+import sourcemaps from 'gulp-sourcemaps';
+
+export default function defaultTask(cb) {
 	console.log('This is a first task')
 	cb();
 }
 
-exports.default = defaultTask;
+export function styles() {
+	return src('src/styles/**/*.css')
+		.pipe(sourcemaps.init())
+		.pipe(cleanCSS({debug: true}, (details) => {
+			console.log(`Файл: ${details.name}`);
+			console.log(`Изначальный размер: ${details.stats.originalSize}`);
+			console.log(`Размер после сжатия: ${details.stats.minifiedSize}`);
+			console.log(`Эффективность сжатия: ${Math.round(details.stats.efficiency * 100)}%`)
+			console.log('=============================================')
+		}))
+		.pipe(sourcemaps.write())
+		.pipe(dest('dist/styles'))
+}
